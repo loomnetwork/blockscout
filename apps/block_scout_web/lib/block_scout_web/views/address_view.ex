c38a62cc91f951ef6c9e48c0625454469a22f781
Eduardo Pereira <eduardonunesp@gmail.com>
2019-04-30 19:32:06 -0300

    loom prefix

--- apps/block_scout_web/lib/block_scout_web/views/address_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_view.ex
@@ -213,6 +213,11 @@ defmodule BlockScoutWeb.AddressView do
 
   def token_title(%Token{name: name, symbol: symbol}), do: "#{name} (#{symbol})"
 
+  def loomnify(address) do
+    last = String.length("#{address}") - 1
+    "loom#{String.slice("#{address}", 2..last)}"
+  end
+
   def incoming_transaction_count(%Address{} = address) do
     count = Chain.address_to_incoming_transaction_count(address)
 
