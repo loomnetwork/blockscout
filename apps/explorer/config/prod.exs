d5df1abab9810d8933b445e89c66b503dddef292
Eduardo Pereira <eduardonunesp@gmail.com>
2019-05-25 15:39:01 -0300

    Added queue interval variable

--- apps/explorer/config/prod.exs
+++ apps/explorer/config/prod.exs
@@ -6,8 +6,22 @@ config :explorer, Explorer.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: String.equivalent?(System.get_env("ECTO_USE_SSL") || "true", "true"),
   queue_target: String.to_integer(System.get_env("QUEUE_TARGET") || "50"),
+  queue_interval: String.to_integer(System.get_env("QUEUE_INTERVAL") || "1000"),
   prepare: :unnamed,
   timeout: :timer.seconds(60)
+
+# Our goal is to stay under `:queue_target` for `:queue_interval`.
+# In case we can't reach that, then we double the :queue_target.
+# If we go above that, then we start dropping messages.
+
+# For example, by default our queue time is 50ms. If we stay above
+# 50ms for a whole second (1000ms), we double the target to 100ms and we
+# start dropping messages once it goes above the new limit.
+
+# This allows us to better plan for overloads as we can refuse
+# requests before they are sent to the database, which would
+# otherwise increase the burden on the database, making the
+# overload worse.
 
 config :explorer, Explorer.Tracer, env: "production", disabled?: true
 
