957d025eeaabaca44f445aa1c81372ace3dce626
Eduardo Pereira <eduardonunesp@gmail.com>
2019-05-29 11:12:19 -0300

    Reject receipt params with invalid tx hash

--- apps/indexer/lib/indexer/block/fetcher/receipts.ex
+++ apps/indexer/lib/indexer/block/fetcher/receipts.ex
@@ -37,6 +37,7 @@ defmodule Indexer.Block.Fetcher.Receipts do
   end
 
   def put(transactions_params, receipts_params) when is_list(transactions_params) and is_list(receipts_params) do
+    receipts_params = Enum.reject(receipts_params, &(&1.transaction_hash == "0x0"))
     transaction_hash_to_receipt_params =
       Enum.into(receipts_params, %{}, fn %{transaction_hash: transaction_hash} = receipt_params ->
         {transaction_hash, receipt_params}
