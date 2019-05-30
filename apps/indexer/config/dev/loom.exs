82317d90dfa7df69ef57e6ef1faa12bf02ff3f47
Eduardo Pereira <eduardonunesp@gmail.com>
2019-05-30 16:36:43 -0300

    Added loom configuration

---
+++ apps/indexer/config/dev/loom.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+config :indexer,
+  # Block interval for loomchain is 1 second
+  # Let 5 to not overload the server
+  block_interval: :timer.seconds(5),
+  json_rpc_named_arguments: [
+    transport: EthereumJSONRPC.HTTP,
+    transport_options: [
+      http: EthereumJSONRPC.HTTP.HTTPoison,
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://localhost:46658/eth",
+      http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
+    ],
+    variant: EthereumJSONRPC.Geth
+  ],
+  subscribe_named_arguments: [
+    transport: EthereumJSONRPC.WebSocket,
+    transport_options: [
+      web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://localhost:46658/eth"
+    ]
+  ]
