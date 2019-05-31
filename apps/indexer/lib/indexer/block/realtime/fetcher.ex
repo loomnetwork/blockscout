2ac6b8039e72d36e559b941848601f9f1de85636
bikash <bikash@loomx.io>
2019-05-31 13:46:18 +0530

    remove unused import

--- apps/indexer/lib/indexer/block/realtime/fetcher.ex
+++ apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -20,8 +20,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
       async_import_tokens: 1,
       async_import_token_balances: 1,
       async_import_uncles: 1,
-      fetch_and_import_range: 2,
-      async_import_staking_pools: 0
+      fetch_and_import_range: 2
     ]
 
   alias Ecto.Changeset
