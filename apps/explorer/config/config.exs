aed25b990f420ec5d8e6630e28d2a7745d9bd347
Vadim Macagon <vadim.macagon@gmail.com>
2019-10-26 17:31:20 +0700

    Revert a couple of unnecessary changes

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # General application configuration
 config :explorer,
   ecto_repos: [Explorer.Repo],
-  coin: System.get_env("COIN") || "ETH",
+  coin: System.get_env("COIN") || "POA",
   token_functions_reader_max_retries: 3,
   allowed_evm_versions:
     System.get_env("ALLOWED_EVM_VERSIONS") ||
