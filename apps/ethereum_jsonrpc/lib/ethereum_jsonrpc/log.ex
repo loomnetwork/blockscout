cb1f568831f9677bfe1ac0455247fd288e3ead65
Vadim Macagon <vadim.macagon@gmail.com>
2019-11-06 12:34:03 +0700

    Add comment

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
@@ -162,6 +162,9 @@ defmodule EthereumJSONRPC.Log do
         "transactionIndex" => 0
       }
 
+  Loom includes a `"blockTime"` key whose value could be converted to an integer, but since it's not
+  used by Blockscout it's just kept untransformed (thought it should probably be discarded entirely).
+
   """
   def to_elixir(log) when is_map(log) do
     Enum.into(log, %{}, &entry_to_elixir/1)
